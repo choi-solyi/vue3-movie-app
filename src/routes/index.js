a3230062be79ec07dc057d83a7e2d174afd1d3cb
choi-solyi <choi-solyi@naver.com>
2021-10-19 23:49:38 +0900

    create header(nav)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { createRouter , createWebHashHistory } from 'vue-router'
 import Home from './Home'
+import Movie from './Movie'
 import About from './About'
-
 
 export default createRouter({
   // 1. hash 모드   ~~~주소~~~ /#/search
@@ -12,10 +12,14 @@ export default createRouter({
       //google.com/ <가장 메인 페이지
       path: '/',
       component: Home
-    },{
+    },
+    {
+      path: '/movie',
+      component: Movie
+    },
+    {
       path: '/about',
       component: About
     }
   ]
-
 })
