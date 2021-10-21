e1ae62da2a2b44b90f5284a9175eb7ded8d4ba54
choi-solyi <choi-solyi@naver.com>
2021-10-21 17:20:12 +0900

    api 연동 비동기 처리 (코드 리팩토링)

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { rest } from 'lodash'
 import _uniqBy from 'lodash/uniqBy'
 
 export default {
@@ -32,35 +33,67 @@ export default {
   },
   //methods - 변경 허용 X  비동기로 처리가 되도록 되어있음
   actions: {
-    async searchMovies({state, commit}, payload){
-      const { title, type, number, year } = payload
-      const OMDB_API_KEY = 'ab697f6f'
-      
-      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
-
-      const { Search, totalResults } = res.data
-      commit('updateState', {
-        movies: _uniqBy(Search, 'imdbID') 
-      })
-      console.log(totalResults) //frozen 268개
-      console.log(typeof totalResults)  //string
-
-      const total = parseInt(totalResults, 10)
-      const pageLength = Math.ceil( total / 10)
-
-      //추가 요청 전송
-      if( pageLength > 1){
-        for(let page = 2; page <= pageLength; page++){
-          if( page > (number / 10) ) break
-          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
-          const { Search } = res.data
-          commit('updateState', {
-            movies: [
-              ...state.movies, ..._uniqBy(Search, 'imdbID')
-            ]
-          })
+    async searchMovies({ state, commit }, payload){
+      try {
+        const res = await _fetcheMove({
+          ...payload,
+          page: 1
+        })
+        const { Search, totalResults } = res.data
+        commit('updateState', {
+          movies: _uniqBy(Search, 'imdbID') 
+        })
+        console.log(totalResults) //frozen 268개
+        console.log(typeof totalResults)  //string
+  
+        const total = parseInt(totalResults, 10)
+        const pageLength = Math.ceil( total / 10)
+  
+        //추가 요청 전송
+        if( pageLength > 1){
+          for(let page = 2; page <= pageLength; page++){
+            if( page > (payload.number / 10) ) break
+            const res = await _fetcheMove({
+              ...payload,
+              page: page  //page <라고 생략가능
+            })
+            const { Search } = res.data
+            commit('updateState', {
+              movies: [
+                ...state.movies, ..._uniqBy(Search, 'imdbID')
+              ]
+            })
+          }
         }
+      } catch (err) {
+        commit('updateState', {
+          movies: [],
+          message: err
+        })
       }
     }
   }
+}
+
+function _fetcheMove(payload){  // 언더바 == 여기서만 쓸꺼임
+ const { title, type, year, page } = payload
+ const OMDB_API_KEY = 'ab697f6f'
+ const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+//  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}`
+
+ return new Promise((resolve, reject) => { 
+   axios.get(url)
+    .then(res => {
+      // omdb의 경우 에러가 발생해도 200 코드를 반환하는 경우가 있으므로
+      // 따로 예외처리를 해주어야 한다
+      if(res.data.Error){       
+        reject(res.data.Error)
+        return
+      }
+      resolve(res)
+    })
+    .catch(err => {
+      reject(err.message)
+    })
+ })
 }
